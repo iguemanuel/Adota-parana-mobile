f42a3fb2410501e1dc1673254588ccbffad3545d
Igor Emanuel <igor17emanuel@gmail.com>
2025-05-27 20:36:54 -0300

    Add colors for app

--- adota-parana-mobile/constants/Colors.ts
+++ adota-parana-mobile/constants/Colors.ts
@@ -1,19 +1,13 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
-
-export default {
-  light: {
-    text: '#000',
-    background: '#fff',
-    tint: tintColorLight,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorLight,
-  },
-  dark: {
-    text: '#fff',
-    background: '#000',
-    tint: tintColorDark,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorDark,
-  },
+export const Colors = {
+  primary: "#F97316",
+  secondary: "#1583AA",
+  background: "#FFFFFF",
+  textPrimary: "#111827",
+  textSecondary: "#6B7280",
+  border: "#D1D5DB",
+  success: "#10B981",
+  warning: "#F59E0B",
+  error: "#EF4444",
+  info: "#3B82F6",
+  light: "#F3F4F6",
 };
